7426f1fd96374c7dc9ebdf2113b764ce6fb20828
Taciano <maximotco@hotmail.com>
2023-04-15 14:54:32 -0300

    Update EmptyView.swift

--- EvercodeApp/Screens/Components/EmptyView.swift
+++ EvercodeApp/Screens/Components/EmptyView.swift
@@ -3,4 +3,36 @@ import UIKit
 class EmptyView: UIView {
     
     // TODO
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "No data found"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .semibold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        return nil
+    }
+    
+    private func commonUI() {
+        
+        backgroundColor = .white
+        addSubview(label)
+        
+        subviews.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
 }
